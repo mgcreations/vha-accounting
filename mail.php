e186fab214f4e125c7152a506055771ef3a3d182
Michael Ashworth <info@mgcreations.co.za>
2020-01-16 05:06:50 +0200

    Final code changes, launching site

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@
 
     $formcontent = "From: $name \n Email: $email \n Phone: $phone \n Message: $message";
 
-    $recipient = "info@mgcreations.co.za";
+    $recipient = "info@vhaaccounting.com";
     $subject = "Enquiry for Website";
     $mailheader = "From: $email \r\n";
 
@@ -15,6 +15,6 @@
 
     if (!$_POST['submit'] == 'Submit')
     {
-        header('Location: http://www.vhaaccounting.co.za/index.html');
+        header('Location: http://www.vhaaccounting.co.za/thank_you.html');
     }
 ?>
