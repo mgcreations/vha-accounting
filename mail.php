fc797d5365438487fdd1368b784a65b1bd369053
MG Creations <info@mgcreations.co.za>
2020-08-26 21:35:47 +0200

    Debugging contact form

--- mail.php
+++ mail.php
@@ -1,20 +1,15 @@
 <?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+$message = $_POST['message'];
 
-    $formcontent = "From: $name \n Email: $email \n Phone: $phone \n Message: $message";
+$formcontent=" From: $name \n Phone: $phone \n Message: $message";
 
-    $recipient = "info@vhaaccounting.com";
-    $subject = "Enquiry for Website";
-    $mailheader = "From: $email \r\n";
-
-    mail($recipient,$subject,$formcontent,$mailheader) or die("Error!");
-
-    if (!$_POST['submit'] == 'Submit')
-    {
-        header('Location: http://www.vhaaccounting.co.za/thank_you.html');
-    }
+$recipient = "info@vhaaccounting.com";
+$subject = "Enquiry from Website";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
